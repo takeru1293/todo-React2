ff3aa0bf5601e4acfdd126fe77d9618199912a66
takeru1293 <68981576+takeru1293@users.noreply.github.com>
2021-10-29 17:33:06 +0900

    style追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import ReactDom from "react-dom";
 
 const App = () => {
-  const onClickButton = () => {
-    alert();
+  const onClickButton = () => alert();
+  const constentStyle = {
+    color: "blue",
+    fondSize: "18px"
   };
 
   return (
     <>
-      <h1>こんにちは</h1>
-      <p>元気ですか?</p>
+      <h1 style={{ color: "red" }}>こんにちは</h1>
+      <p style={constentStyle}>元気ですか?</p>
       <button onClick={onClickButton}>ボタン</button>
     </>
   );
