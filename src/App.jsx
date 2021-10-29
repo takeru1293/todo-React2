f101de9a681fbf1e03ced6ec89b96b8f196aee06
takeru1293 <68981576+takeru1293@users.noreply.github.com>
2021-10-29 19:05:32 +0900

    eslintの追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+/* eslint react-hooks/exhaustive-deps: */
+
 import React, { useEffect, useState } from "react";
 import ReactDom from "react-dom";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
   // stateの設定----------------------------------------------------------
+
   const [num, setNum] = useState(0);
+
   const [faceShowFlag, setFaceShowFlag] = useState(false);
 
   // 処理内容----------------------------------------------------------
@@ -17,22 +21,23 @@ const App = () => {
     setFaceShowFlag(!faceShowFlag);
   };
 
-  // useEffectの設定、第二引数に空の場合最初の一回だけ実装
+  //useEffectの設定、-----------------------------------------------
+  // 第二引数に空の場合最初の一回だけ実装
   useEffect(() => {
     console.log("useEffect1");
   }, []);
   // 第二引数にnumを設定した場合、numのみに関心を持つ→numの値が変更された場合のみ実行
   useEffect(() => {
-    console.log("useEffect2");
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [num]);
 
-  if (num > 0) {
-    if (num % 3 === 0) {
-      faceShowFlag || setFaceShowFlag(true);
-    } else {
-      faceShowFlag && setFaceShowFlag(false);
-    }
-  }
   // 画面内容----------------------------------------------------------
   return (
     <>
