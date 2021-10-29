ae2d0c690fd080e4915deee74227f78c8a7d1004
takeru1293 <68981576+takeru1293@users.noreply.github.com>
2021-10-29 17:29:40 +0900

    coChickイベントの追加

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDom from "react-dom";
 
 const App = () => {
+  const onClickButton = () => {
+    alert();
+  };
+
   return (
     <>
       <h1>こんにちは</h1>
       <p>元気ですか?</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-import App from "./App.js";
+import App from "./App.jsx";
 
 // コンポーネント化下ファイルを出力
 ReactDom.render(<App />, document.getElementById("root"));
