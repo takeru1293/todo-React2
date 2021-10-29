04af0c7bd18c7d1e2b2b763c063e12626f666c28
takeru1293 <68981576+takeru1293@users.noreply.github.com>
2021-10-29 18:47:32 +0900

    useStateの追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  // stateの設定----------------------------------------------------------
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
 
+  // 処理内容----------------------------------------------------------
+  // カウントアップ処理
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  };
+  // フラグの出し分け
+  const onClickSwichShow = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+  // 画面内容----------------------------------------------------------
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="pink">元気です。</ColorfulMessage>
-      <button onClick={onClickButton}>ボタン</button>
+      <button onClick={onClickCountUp}>カウントアップ</button>
+
+      <br />
+
+      <button onClick={onClickSwichShow}>on off</button>
+      <p>{num}</p>
+      {faceShowFlag && <p>( ??)</p>}
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 const ColorfulMessage = (props) => {
-  console.log(props);
   const { color, children } = props;
   const constentStyle = {
     color,
